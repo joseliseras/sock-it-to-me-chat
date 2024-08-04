d251176d6f9b3c4f84a0f37e29e11f9ad82e961e
joseliseras <jliseras@gmail.com>
2024-08-04 11:34:05 -0400

    Sock it to me chat1

--- servidor_chat.py
+++ servidor_chat.py
@@ -27,7 +27,7 @@ try:
                     # Recibir mensajes de los clientes
                     mensaje = sock.recv(1024)
                     if mensaje:
-                        # Hacer broadcast del mensaje a todos los demás clientes
+                        # Hacer broadcast(transmision) del mensaje a todos los demás clientes
                         for conexion in conexiones:
                             if conexion != servidor and conexion != sock:
                                 try:
